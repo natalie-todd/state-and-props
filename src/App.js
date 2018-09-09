cda1a5c7bfbdaeec4f6e422cea719e037e66d5a0
Natalie <natalie.todd15@gmail.com>
2018-09-09 14:50:47 -0600

    change method working

--- src/App.js
+++ src/App.js
@@ -11,15 +11,26 @@ class App extends Component {
     ]
   }
 
+  unChangedHandler = (newUN) => {
+    this.setState({
+      persons: [
+        { un: newUN },
+        { un: 'catMom666' },
+        { un: 'eennaaaaneee' }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
         </header>
         <main>
-          <Out un={this.state.persons[0].un}/>
-          <Out un={this.state.persons[1].un}>I love Cats!</Out>
-          <Out un={this.state.persons[2].un}/>
+          <Out un={this.state.persons[0].un} />
+          <Out un={this.state.persons[1].un}
+          click={this.unChangedHandler.bind(this, 'RachelSarah!')}>I love Cats!</Out>
+          <Out un={this.state.persons[2].un} />
         </main>
       </div>
     );
--- src/Out/Out.js
+++ src/Out/Out.js
@@ -5,7 +5,7 @@ import './Out.css';
 const Out = (props) => {
     return (
         <div className='Out'>
-            <p>I'm Natalie and my username is {props.un}!</p>
+            <p onClick={props.click}>I'm Natalie and my username is {props.un}!</p>
             <p>{props.children}</p>
         </div>
     )
